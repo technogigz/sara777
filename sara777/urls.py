ae990980627eb94bfcd3c6517d7e62503b5df817
Technogigz <technogigzofficial@gmail.com>
2024-03-13 00:57:13 +0530

    sc

--- sara777/urls.py
+++ sara777/urls.py
@@ -23,9 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('sara.urls')),
 
-]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
