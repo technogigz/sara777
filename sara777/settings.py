0cb3536e8c316c4890fda4d9a0947d18a5f025c1
Technogigz <technogigzofficial@gmail.com>
2024-03-13 13:31:51 +0530

    Initial commit

--- sara777/settings.py
+++ sara777/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
